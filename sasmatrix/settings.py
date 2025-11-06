840f5774cfbabe2a0593d888822104f53fb7b9be
Mohamedarif-web <mohamedarifm12@gmail.com>
2025-11-06 18:58:57 +0530

    Update settings.py

--- sasmatrix/settings.py
+++ sasmatrix/settings.py
@@ -114,12 +114,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-#STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',  # Your static files folder
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',  # Your static files folder
+# ]
 
 
 # Default primary key field type
